a808bf90039ded4964461cac0a18f8ab49283f1e
Juan Carlos Gomez <eslost07@gmail.com>
2023-07-01 20:03:43 -0500

    primer commit

---
+++ src/data/menuItems.ts
@@ -0,0 +1,22 @@
+import type { MenuItem } from "../types/types";
+
+const menuItems: MenuItem[] = [
+    {
+        title: 'Home',
+        url: '/'
+    },
+    {
+        title: 'information',
+        url: '/information'
+    },
+    {
+        title: 'projects',
+        url: '/projects'
+    },
+    {
+        title: 'contact',
+        url: '/contact'
+    }
+]
+
+export default menuItems;
